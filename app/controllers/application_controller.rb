767f1669a721dbf7052d3a87e5137ba6f7f61171
Kevin Trinh <kxt.trinh@gmail.com>
2022-10-24 15:57:45 +0200

    Post + faker + will_paginate + friendly_id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  helper_method :admin?
+
+  private
+
+  def admin?
+    # redirect_to root_path unless current_user && current_user.admin
+    redirect_to root_path unless current_user&.admin # pro syntax mdr
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "open-uri"
 
+User.destroy_all
+Post.destroy_all
 
 User.create!(email: "kevin@mail.com",
- password: "123456",
- admin: true)
+  password: "123456",
+  admin: true)
+
+25.times do |i|
+  post = Post.new
+  post.title = Faker::Lorem.sentence(word_count: 3, random_words_to_add: 7)
+  post.body = Faker::Lorem.paragraph_by_chars(number: 1500)
+  post.user = User.first
+  file = URI.open('https://picsum.photos/1920/1080')
+  post.thumbnail.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+  file = URI.open('https://picsum.photos/1920/1080')
+  post.banner.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+  # post.thumbnail.attach(io: open("https://picsum.photos/1920/1080"), filename: "#{i}_thumbnail.jpg")
+  # post.banner.attach(io: open("https://picsum.photos/1920/1080"), filename: "#{i}_banner.jpg")
+  post.views = Faker::Number.between(from: 1, to: 5000)
+  post.save!
+end
 
  puts "Finished !"
